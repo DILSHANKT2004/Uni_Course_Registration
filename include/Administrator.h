6ca0e9373593b38ae1d545b46218d46eee6d56f1
DILSHANKT2004 <en121394@foe.sjp.ac.lk>
2026-09-08 23:23:49 +0530

    Update Administrator.h

--- include/Administrator.h
+++ include/Administrator.h
@@ -1,11 +1,13 @@
 #ifndef ADMINISTRATOR_H
 #define ADMINISTRATOR_H
-
+#include <string>
 #include "User.h"
+using namespace std;
+
 
 class Administrator : public User {
 public:
-    Administrator(std::string uname, std::string pass, std::string name);
+    Administrator(string uname, string pass, string name);
     
     void displayMenu() const override;
 };
